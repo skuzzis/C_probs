7d9a223f13743f9f7ac72758ca98c44d6c06397e
Andrei Sebastian <andrei.sava@stud.etti.upb.ro>
2024-11-10 21:23:24 +0200

    Performance PTSD

--- 3.10.c
+++ 3.10.c
@@ -3,9 +3,7 @@ int main() {
     int n,k=0,s=0,p,r;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        s=s+i*i;
-    }
-    for(int i=1;i<=n;i++) {
+        s += (i*i);
         k +=i;
     }
     p=k*k;
